c81ba5a42f8f2cb966aed7aa0a04d0ba3eb5cf27
allavi <46686049+allavi@users.noreply.github.com>
2019-01-18 21:39:50 +0300

    little changes

--- func_def.svh
+++ func_def.svh
@@ -1,18 +1,7 @@
-// a parameterized function
-
+// to use this functions with any vector`s width
 `define onehot2bin(vector)  func_pack::func_pkg #(.SIZE($size(vector)))::onehot2bin(vector)
 `define bin2onehot(vector)  func_pack::func_pkg #(.SIZE($size(vector)))::bin2onehot(vector)
 `define maj_func(vector)    func_pack::func_pkg #(.SIZE($size(vector)))::maj_func(vector)
 `define bit_reverse(vector) func_pack::func_pkg #(.SIZE($size(vector)))::bit_reverse(vector)
 `define bin2grey(vector)    func_pack::func_pkg #(.SIZE($size(vector)))::bin2grey(vector)
 `define grey2bin(vector)    func_pack::func_pkg #(.SIZE($size(vector)))::grey2bin(vector)
-
-module functions_use (
-   input clk   ,
-   input clk_en,
-   input rst_n
-);
-
-
-
-endmodule
