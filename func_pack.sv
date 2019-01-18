a9515b48740d0d259b3543570d6c38a3b6e816d9
allavi <46686049+allavi@users.noreply.github.com>
2019-01-18 21:40:58 +0300

    little changes

--- func_pack.sv
+++ func_pack.sv
@@ -18,12 +18,12 @@ package func_pack;
 			end
 		endfunction
 
-      static function logic [(2*SIZE)-1:0] bin2onehot(logic [SIZE-1:0] bin_vector, bit hot_one = 1);
-         if(hot_one)
-            bin_to_one_hot = 1<<bin_vector+1;
-         else
-            bin_vector = ~(1<<bin_vector+1);
-      endfunction
+		static function logic [(2*SIZE)-1:0] bin2onehot(logic [SIZE-1:0] bin_vector, bit hot_one = 1);
+			if(hot_one)
+				bin_to_one_hot = 1<<bin_vector+1;
+			else
+				bin_vector = ~(1<<bin_vector+1);
+		endfunction
 
 		static function logic [MASTERS*SLAVES-1:0] transponse (logic [SLAVES-1:0][MASTERS-1:0] bit_vector);
 			logic [MASTERS-1:0][SLAVES-1:0] vector;
@@ -33,35 +33,35 @@ package func_pack;
 			return vector;
 		endfunction
 
-      static function logic maj_func ([SIZE-1:0]vector);
-         int sum=0;
-         for (int i = 0; i < SIZE; i++)
-            sum += vector[i];
-         return sum>((SIZE%2)?((SIZE-1)/2):(SIZE/2));
-      endfunction
+		static function logic maj_func ([SIZE-1:0]vector);
+			int sum=0;
+			for (int i = 0; i < SIZE; i++)
+				sum += vector[i];
+			return sum>((SIZE%2)?((SIZE-1)/2):(SIZE/2));
+		endfunction
 
-      static function logic [SIZE-1:0] bit_reverse ([SIZE-1:0]vector);
-         logic [SIZE-1:0] rev;
-         for (int i = 0; i < SIZE; i++)
-            rev[SIZE-i] += vector[i];
-         return rev;
-      endfunction
+		static function logic [SIZE-1:0] bit_reverse ([SIZE-1:0]vector);
+			logic [SIZE-1:0] rev;
+			for (int i = 0; i < SIZE; i++)
+				rev[SIZE-i] += vector[i];
+			return rev;
+		endfunction
 
-      static function logic [SIZE-1:0] bin2grey ([SIZE-1:0]bin_vector);
-         logic [SIZE-1:0] grey_vector;
-         grey_vector[SIZE-1] = bin_vector[SIZE-1];
-         for (int i = 0; i < SIZE-1; i++) begin
-            grey_vector[i] = bin_vector[i]^bin_vector[i+1];
-         end
-      endfunction
+		static function logic [SIZE-1:0] bin2grey ([SIZE-1:0]bin_vector);
+			logic [SIZE-1:0] grey_vector;
+			grey_vector[SIZE-1] = bin_vector[SIZE-1];
+			for (int i = 0; i < SIZE-1; i++) begin
+				grey_vector[i] = bin_vector[i]^bin_vector[i+1];
+			end
+		endfunction
 
-      static function logic [SIZE-1:0] grey2bin ([SIZE-1:0]grey_vector);
-         logic [SIZE-1:0] bin_vector;
-         bin_vector[SIZE-1] = grey_vector[SIZE-1];
-         for (int i = 0; i < SIZE-1; i++) begin
-            bin_vector[i] = bin_vector[i+1]^grey_vector[i];
-         end
-      endfunction
+		static function logic [SIZE-1:0] grey2bin ([SIZE-1:0]grey_vector);
+			logic [SIZE-1:0] bin_vector;
+			bin_vector[SIZE-1] = grey_vector[SIZE-1];
+			for (int i = 0; i < SIZE-1; i++) begin
+				bin_vector[i] = bin_vector[i+1]^grey_vector[i];
+			end
+		endfunction
 
 	endclass
 
